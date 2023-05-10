8274fc1d78d685caf43ce5799672d5d3099c5e3c
William Connell <williamconnell@williams-mbp.lan>
2023-05-09 21:13:04 -0500

    added minilogue xd image asset

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img
+    alt="Vue logo"
+    style="width: 500px"
+    src="./assets/miniloguexd.png"
+  /><br />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
